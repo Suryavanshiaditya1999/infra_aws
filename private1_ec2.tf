6f121f46386b816a321e3fac79ea2c85fd3e8031
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-05-18 16:25:26 +0530

    Update private1_ec2.tf

--- private1_ec2.tf
+++ private1_ec2.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "deploy_ec2_private1_instance" {
   key_name = "deploy"
 
   tags = {
-    Name = " deploy-private1-ec2"
+    Name = "kibana1"
   }
 
   # associate_public_ip_address = false
